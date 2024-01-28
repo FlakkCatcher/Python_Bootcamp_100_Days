85a3b6b03cdae848f7b0825a7d2fd19bd70a9112
FlakkCatcher <118646660+FlakkCatcher@users.noreply.github.com>
2024-01-28 11:48:40 -0500

    Create Day 1 - 1.Printing.py

---
+++ Day 1 - 1.Printing.py
@@ -0,0 +1,7 @@
+print("Hello world!\nHello World!\nHello world!")
+print()
+print("Hello" +" Justin")
+print()
+# Always try and include some comments to help you remember this stuff
+print("Hello " + input("What is your name? ") + "!")
+# Don't forget that you can turn an entire line of code into a comment with Ctrl +/
