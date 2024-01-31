719d618692980d522b951125150f2831fe50b8a4
FlakkCatcher <jgoodenkauf@gmail.com>
2024-01-30 22:31:47 -0500

    renamed

---
+++ Day 3 - 1. Control Flow and Conditional Operators.py
@@ -0,0 +1,11 @@
+# IF-ELSE STATEMENTS AND CONDITIONAL OPERATIONS
+
+# Using if-else statements is a way to divide data into categories based on its attributes
+print("Welcome to the roller coaster!")
+height = int(input("What is your height in cm? "))
+if height >= 120:
+    print("You can ride the roller coaster!")     # The indentation says that this line acts underneath the above one
+else:
+    print("Sorry, you have to grow taller before you can ride.")
+
+# You can use other comparison operators, such as >, <, >=, <=, ==, !=, and %
---
+++ Day 3 - 2. Nested IF and ELIF Statements.py
@@ -0,0 +1,17 @@
+# NESTED IF AND ELIF STATEMENTS
+
+# Nesting refers to putting conditional statements inside of conditional statements
+print("Welcome to the roller coaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You can ride the roller coaster!")
+    age = int(input("What is your age? "))
+    if age < 12:
+        print("Please pay $5.")
+    elif 12 < age < 18:
+        print("Please pay $7.")
+    else:
+        print("Please pay $12.")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
