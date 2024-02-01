03a8f10359153e90609322613a0a4c289960c819
FlakkCatcher <jgoodenkauf@gmail.com>
2024-01-31 22:27:34 -0500

    renamed

--- Test.py
+++ Test.py
@@ -1,3 +1,13 @@
-#This is a test file for PyCharm Push/Pull Requests
+# This is a test file for PyCharm Push/Pull Requests
 print("Hello, PyCharm!")
 
+year = int(input())
+if year % 400 == 0:
+    print("Leap year")
+elif year % 4 == 0:
+    if year % 100 == 0:
+        print("Not leap year")
+    elif year % 100 != 0:
+        print("Leap year")
+else:
+    print("Not leap year")
